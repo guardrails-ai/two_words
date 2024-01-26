72fde28bd15759db4d7ee5b094c536cd0da1f33a
Safeer Mohiuddin <safeer@guardrailsai.com>
2024-01-26 01:18:37 +0000

    uploading two words validator

--- validator/main.py
+++ validator/main.py
@@ -1,9 +1,8 @@
-import re
-import string
-from typing import Any, Callable, Dict, Optional
+from typing import Any, Dict
 
-import rstr
+from pydash.strings import words as _words
 
+from guardrails.logger import logger
 from guardrails.validator_base import (
     FailResult,
     PassResult,
@@ -13,59 +12,37 @@ from guardrails.validator_base import (
 )
 
 
-@register_validator(name="guardrails/regex_match", data_type="string")
-class RegexMatch(Validator):
-    """Validates that a value matches a regular expression.
+@register_validator(name="guardrails/two-words", data_type="string")
+class TwoWords(Validator):
+    """Validates that a value is two words.
 
     **Key Properties**
 
     | Property                      | Description                       |
     | ----------------------------- | --------------------------------- |
-    | Name for `format` attribute   | `regex_match`                     |
+    | Name for `format` attribute   | `two-words`                       |
     | Supported data types          | `string`                          |
-    | Programmatic fix              | Generate a string that matches the regular expression |
+    | Programmatic fix              | Pick the first two words.         |
+    """
 
-    Args:
-        regex: Str regex pattern
-        match_type: Str in {"search", "fullmatch"} for a regex search or full-match option
-    """  # noqa
+    def _get_fix_value(self, value: str) -> str:
+        words = value.split()
+        if len(words) == 1:
+            words = _words(value)
 
-    def __init__(
-        self,
-        regex: str,
-        match_type: Optional[str] = None,
-        on_fail: Optional[Callable] = None,
-    ):
-        # todo -> something forces this to be passed as kwargs and therefore xml-ized.
-        # match_types = ["fullmatch", "search"]
+        if len(words) == 1:
+            value = f"{value} {value}"
+            words = value.split()
 
-        if match_type is None:
-            match_type = "fullmatch"
-        assert match_type in [
-            "fullmatch",
-            "search",
-        ], 'match_type must be in ["fullmatch", "search"]'
-
-        super().__init__(on_fail=on_fail, match_type=match_type, regex=regex)
-        self._regex = regex
-        self._match_type = match_type
+        return " ".join(words[:2])
 
     def validate(self, value: Any, metadata: Dict) -> ValidationResult:
-        p = re.compile(self._regex)
-        """Validates that value matches the provided regular expression."""
-        # Pad matching string on either side for fix
-        # example if we are performing a regex search
-        str_padding = (
-            "" if self._match_type == "fullmatch" else rstr.rstr(string.ascii_lowercase)
-        )
-        self._fix_str = str_padding + rstr.xeger(self._regex) + str_padding
+        logger.debug(f"Validating {value} is two words...")
 
-        if not getattr(p, self._match_type)(value):
+        if len(value.split()) != 2:
             return FailResult(
-                error_message=f"Result must match {self._regex}",
-                fix_value=self._fix_str,
+                error_message="must be exactly two words",
+                fix_value=self._get_fix_value(str(value)),
             )
-        return PassResult()
 
-    def to_prompt(self, with_keywords: bool = True) -> str:
-        return "results should match " + self._regex
+        return PassResult()
